e7961d3bcdf4e2c079ac9511c6144cf4f964de97
88BlackAndWhite <heeeenry@gmail.com>
2018-07-15 14:04:05 -0400

    uploaded file

---
+++ Cryptocurrencies/BTC_segwit_percent.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 12:52:26 2018
+"""
+
+segwit_url = ("https://api.blockchair.com/bitcoin/blocks?q=time(2017-09-01..)"+
+              "&u=ccaf6606-c3ab-4124-a2f2-1ce6564bbc54&fields="+
+              "id,time,transaction_count,witness_count&export=csv")
+
+import pandas as pd
+Table_segwit = pd.read_csv(segwit_url, delimiter=',')
+
+#%%
+Table_segwit['s_to_t_ratio'] = (Table_segwit['witness_count']/
+            Table_segwit['transaction_count'])
+
+Table_segwit['ratio_144'] = Table_segwit['s_to_t_ratio'].rolling(
+        144, min_periods = 1, center = True).mean()
+
+#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes = "Dark")
+
+fig0, ax0 = plt.subplots(1,1,figsize = (8,6))
+ax0.plot(Table_segwit['id'],Table_segwit['ratio_144'])

62c91aa57be6d4fe850857525f18ace855b54987
black-wolfie <38986824+black-wolfie@users.noreply.github.com>
2018-08-30 14:28:49 -0400

    updated for new API scheme

--- blockchains-visualizations/BTC_segwit_percent.py
+++ blockchains-visualizations/BTC_segwit_percent.py
@@ -4,13 +4,14 @@ Created on Sun Jul 15 12:52:26 2018
 """
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 def main():
-    segwit_url = ("https://api.blockchair.com/bitcoin/blocks?q=time(2017-09-01..)"+
-                  "&u=ccaf6606-c3ab-4124-a2f2-1ce6564bbc54&fields="+
-                  "id,time,transaction_count,witness_count&export=csv")
+    segwit_url = ("https://alpha-2.blockchair.com/bitcoin/blocks?fields=id,"+
+                  "hash,time,transaction_count,witness_count&q=time(20"+
+                  "17-09-01..)&export=csv")
     
-    import pandas as pd
+    
     Table_segwit = pd.read_csv(segwit_url, delimiter=',')
     
     #%%
